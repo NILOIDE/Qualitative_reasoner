f9908ea701d0d71dab3667ac73a3bdeca8ac5576
NILOIDE <nilstoltanso@gmail.com>
2019-04-07 17:02:43 +0200

    simplified permutations

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import itertools
 
 def init_states():
     # data = read_quantity_input_file()
-    inflow = {'magnitude': ['0', '+'], 'derivative': ['0']}
-    volume = {'magnitude': ['0', '+', 'max'], 'derivative': ['-', '0', '+']}
-    outflow = {'magnitude': ['0', '+'], 'derivative': ['-', '0', '+']}
+    derivative = ['-', '0', '+']
+    inflow = ['0', '+']
+    volume = ['0', '+', 'max']
+    outflow = ['0', '+', 'max']
     quantities_labels = ['inflow', 'volume', 'outflow']
-    quantities = [inflow['magnitude'], inflow['derivative'], volume['magnitude'], volume['derivative'], outflow['magnitude'], outflow['derivative']]
+    quantities = [inflow, derivative, volume, derivative, outflow, derivative]
     permutations = itertools.product(*quantities)
     return permutations
 
 
 def run(args):
-    state = init_states()
+    states = init_states()
+    for p in states:
+        print(p)
 
 
 if __name__ == '__main__':
