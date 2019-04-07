a64737fc5c75705e18a5e6e78ae13b2340b39caa
frank <francidalca@gmail.com>
2019-04-07 21:53:49 +0200

    added next and previous functionality in state

--- state.py
+++ state.py
@@ -14,6 +14,7 @@ class State:
         State.count += 1
         self.inputs = []
         self.outputs = []
+        self.check_validity()
 
     def __str__(self):
         i = "I(" + ', '.join(self.values[0:2]) + '), '
@@ -25,7 +26,8 @@ class State:
         if self.values[0] not in State.i_values or self.values[1] not in State.d_values\
                 or self.values[2] not in State.ov_values or self.values[3] not in State.d_values\
                 or self.values[4] not in State.ov_values or self.values[5] not in State.d_values:
-            print("WARNING: illegal state")
+            print("\nWARNING: illegal state")
+            print(self, "\n")
 
     def get_id(self):
         return self.id
@@ -41,3 +43,38 @@ class State:
 
     def get_outputs(self):
         return self.outputs
+
+    def is_equal(self, values):
+        return self.values == values
+
+    def next(self, v_idx):
+        v_new = None
+        if v_idx in [1, 3, 5]:
+            d_idx = State.d_values.index(self.values[v_idx])
+            new_idx = max(0, min(d_idx + 1, 2))
+            v_new = State.d_values[new_idx]
+        elif v_idx == 0:
+            i_idx = State.i_values.index(self.values[v_idx])
+            new_idx = max(0, min(i_idx + 1, 1))
+            v_new = State.i_values[new_idx]
+        elif v_idx in [2, 4]:
+            ov_idx = State.ov_values.index(self.values[v_idx])
+            new_idx = max(0, min(ov_idx + 1, 2))
+            v_new = State.ov_values[new_idx]
+        return v_new
+
+    def previous(self, v_idx):
+        v_new = None
+        if v_idx in [1, 3, 5]:
+            d_idx = State.d_values.index(self.values[v_idx])
+            new_idx = max(0, min(d_idx - 1, 2))
+            v_new = State.d_values[new_idx]
+        elif v_idx == 0:
+            i_idx = State.i_values.index(self.values[v_idx])
+            new_idx = max(0, min(i_idx - 1, 1))
+            v_new = State.i_values[new_idx]
+        elif v_idx in [2, 4]:
+            ov_idx = State.ov_values.index(self.values[v_idx])
+            new_idx = max(0, min(ov_idx - 1, 2))
+            v_new = State.ov_values[new_idx]
+        return v_new
